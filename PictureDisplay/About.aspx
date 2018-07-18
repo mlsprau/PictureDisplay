c66868e20aff988ff84fdb99430bb27093176c2a
Michael SprauMichae <mlsprau@outlook.com>
2018-07-18 16:01:03 -0700

    Add project files.

---
+++ PictureDisplay/About.aspx
@@ -0,0 +1,12 @@
+﻿<%@ Page Title="About" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="About.aspx.cs" Inherits="PictureDisplay.About" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+    <h2><%: Title %>.</h2>
+    <h3>Your application description page.</h3>
+    <p>Use this area to provide additional information.</p>
+    <asp:Button ID="btnSubmit" runat="server" Text="Display Image" />
+    <asp:Image ID="image2" runat="server" value ="0"/>
+    <asp:Label ID="Label1" runat="server" Text="Label"></asp:Label>
+    <asp:HiddenField ID="HiddenField1" runat="server" Value="0" />
+
+</asp:Content>
---
+++ PictureDisplay/About.aspx.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace PictureDisplay
+{
+    public partial class About : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+       
+            //btnSubmit.Click += new EventHandler(ImageBtn_Click);
+
+                ImageBtn_Click(sender, e);
+            
+
+        }
+
+        public string filename = "";
+        public int count = 0;
+
+        public void ImageBtn_Click(Object sender, EventArgs e)
+        {
+
+            btnSubmit.Text = "filename";
+            count = Convert.ToInt16(HiddenField1.Value);
+
+
+            count++;
+
+            switch (count)
+            {
+                case 1:
+                    filename = "";
+                    image2.ImageUrl = filename;
+                    break;
+                case 2:
+                    filename = "..\\Content\\what-a-beach.jpg";
+                    image2.ImageUrl = filename;
+                    Label1.Text = count.ToString();
+                    break;
+                case 3:
+                    filename = "..\\Content\\lake.jpg";
+                    image2.ImageUrl = filename;
+                    Label1.Text = count.ToString();
+                    count = 0;
+                    break;
+            }
+
+            HiddenField1.Value = count.ToString();
+
+
+        }
+
+  
+
+
+
+    }
+}
